df115655e379d5973054c8bc4e38981394574879
Wu You <120346213+wuu0417@users.noreply.github.com>
2025-10-19 13:01:28 +0800

    Update Core-Stabilizer.py

--- core/Core-Stabilizer.py
+++ core/Core-Stabilizer.py
@@ -1,1 +1,10 @@
+danger = input("What happened now?")
 
+# Will its danger kill me? Yes or No
+
+judge = input("Will its danger kill me?")
+
+if judge == "No":
+    print("I Fearless!")
+else:
+    print("More considerations...")
