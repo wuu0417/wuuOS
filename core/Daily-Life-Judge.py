bc24582df98cdfa7cb2a4db40118717fecdeaae8
Wu You <120346213+wuu0417@users.noreply.github.com>
2025-10-19 13:08:00 +0800

    Create Daily-Life-Judge.py

---
+++ core/Daily-Life-Judge.py
@@ -0,0 +1,10 @@
+thing = input("What happened now?")
+
+# Is it related to me? Yes or No
+
+judge = input("Is it related to me?")
+
+if judge == "No":
+    print("I don't care.")
+else:
+    print("It depends on the specific situation...")
